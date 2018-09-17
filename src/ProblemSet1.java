4ca67c6a8ab03da44fe0736f45207a3717108e59
William Krska <curse.ka@gmail.com>
2018-09-17 11:33:53 -0400

    completed exercises 1-5

--- src/ProblemSet1.java
+++ src/ProblemSet1.java
@@ -9,19 +9,44 @@
 public class ProblemSet1 {	
 	
 	/**
-	 * Every Java program must have a main method, which is
+	 * Every Java program must have a <code>main</code> method, which is
 	 * where program execution begins.
 	 */
 	
 	public static void main(String[] args) {
 		System.out.println("/// EXERCISE 1 /////////////////////////////////////////////////\n");
+		System.out.println("   ##    ######  ######## ########");
+		System.out.println("  #  #   #     # #        #");
+		System.out.println(" #    #  #     # #        #");
+		System.out.println("######## ######  #        ########");
+		System.out.println("#      # #       #               #");
+		System.out.println("#      # #       #               #");
+		System.out.println("#      # #       ######## ########\n");
 		
 		System.out.println("/// EXERCISE 2 /////////////////////////////////////////////////\n");
+		System.out.println("  /\\     /\\     /\\");
+		System.out.println(" /  \\   /  \\   /  \\");
+		System.out.println("/    \\ /    \\ /    \\");
+		System.out.println("\\    / \\    / \\    /");
+		System.out.println(" \\  /   \\  /   \\  /");
+		System.out.println("  \\/     \\/     \\/\n");
 		
 		System.out.println("/// EXERCISE 3 /////////////////////////////////////////////////\n");
+		System.out.println("   \\\\\\\\\\");
+		System.out.println("  +\"\"\"\"\"+");
+		System.out.println("(|  0 0  |)");
+		System.out.println(" |   ^   |");
+		System.out.println(" |  '-'  |");
+		System.out.println("  +-----+\n");
 		
 		System.out.println("/// EXERCISE 4 /////////////////////////////////////////////////\n");
+		System.out.println("H E L L O");
+		System.out.println("E       A");
+		System.out.println("L       P");
+		System.out.println("L       C");
+		System.out.println("O A P C S\n");
 		
 		System.out.println("/// EXERCISE 5 /////////////////////////////////////////////////\n");
+		System.out.println("\"The answer's quite simple,\" the professor said. \"Just think outside the box!\"");
 	}
 }
